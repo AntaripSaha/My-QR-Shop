99d854972e0a071eeb2d27f6bcbcb202b43249e9
AntaripSaha <antarip15@gmail.com>
2023-06-09 20:38:27 +0600

    ..

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,16 +1,15 @@
-@extends('layouts.front', ['class' => ''])
+@extends('layouts.customLayouts', ['class' => ''])
 
 @section('content')
-    <header class="masthead" style="{{ 'background-image: url('.config('global.restorant_details_cover_image').')' }}">
-        <div class="container h-100">
+    
+        <div class="container h-100 pt-5 mt-5">
             <div class="row h-100 align-items-center">
                 <div class="col-12 text-center">
-                    <h1 class="display-2 page-title">{{ $page->title }}</h1>
-                    
+                    <h2 class="display-2 page-title text-black">{{ $page->title }}</h2>
                 </div>
             </div>
-        </div>
-    </header>
+        </div> 
+ 
     <section class="section">
         <div class="container container-pages">
             <div class="row">
@@ -21,6 +20,8 @@
                 </div>
             </div>
         </div>
-
     </section>
 @endsection
+ 
+ 
+
