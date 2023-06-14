a6ad3e0f9a5ba1fb8d28d7bf3d1c55c35bdba40e
antarip15@gmail.com <antarip15@gmail.com>
2023-06-14 10:10:27 +0600

    ..

--- resources/views/qrsaas/partials/tutorial.blade.php
+++ resources/views/qrsaas/partials/tutorial.blade.php
@@ -17,7 +17,7 @@
         <div class="row justify-content-center  ">
             <div class="col-12 col-md-8 text-center">
             <i class="fas fa-edit mr-2 text-primary ckedit_btn" type="button" style="display: none"></i> <h2 class="h1 font-weight-bolder mb-2 ckedit" key="tutorials" id="tutorials">How Does It Works?</h2>
-            <i class="fas fa-edit mr-2 text-primary ckedit_btn" type="button" style="display: none"></i> <p class="lead ckedit" key="product_description" id="product_description">Boost Your Works by Over 40%. Here's How: </p>
+            <i class="fas fa-edit mr-2 text-primary ckedit_btn" type="button" style="display: none"></i> <p class="lead ckedit" key="product_description" id="product_description">Boost your sales by more than 40% with customized QR codes.</p>
             </div>
         </div>
         <div class="row p-2">
